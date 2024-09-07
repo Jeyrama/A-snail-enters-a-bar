4a2921f29b24292be63501e33d0d076b321b949d
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-09-06 20:53:23 -0700

    Update A-snail-enters-a-bar.js

--- A-snail-enters-a-bar.js
+++ A-snail-enters-a-bar.js
@@ -41,3 +41,42 @@ function canSnailReachEnd(length, speed, lengthIncreases) {
 }
 
 // or
+
+function canSnailReachEnd(length, speed, lengthIncreases) {
+  // params
+  // initial length of the rubber band -- num, could be float, 0.01 <= x <= 1,000,000
+  // speed of the snail -- num, could be float 
+  // the increasing length, how much the length increase every minute
+  
+  // results -- boolean, true if the snail can reach the end and false if can't reach the end within a year
+  
+  // if the length increases > speed the answer will always be false
+  // if the length increases <= speed, calculate the length/speed to get how many minutes the snail moves and multiply it
+  // add the length with the increase rate every minute
+  const minutesInAYear = 60 * 24 * 365;
+  if(lengthIncreases > speed){
+    return false
+  } else if(lengthIncreases <= speed){
+    // to know how many minutes have been spent, if more than a minutesInAYear, the snail didn't make it
+    let minute = 0;
+    let finalIncreases = lengthIncreases - speed 
+    // create a new length variable which will be increased every minute
+    let newLength = length;
+    // calculate the newLength with the finalIncreases to know whether or not snail makes it or not
+    // if dthe newLength ever reach 0, snail made it
+    // if the newLength never reach 0 within a year, the snail didn't make it
+    while(newLength > 0){
+      // if the newLength never reach 0 and the minutes > minutesInAYear, break the loop, snail didn't make it
+      if(minute > minutesInAYear) break
+      newLength += finalIncreases
+      minute++
+    }
+    if(newLength <= 0){
+      console.log(finalIncreases, newLength)
+      return true
+    } else {
+      console.log(finalIncreases, newLength)
+      return false
+    }
+  } 
+}
