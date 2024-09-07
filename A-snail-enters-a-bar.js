7a9e9606206a301e95acede58d71c20f0d189d5a
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-09-06 20:52:55 -0700

    Update A-snail-enters-a-bar.js

--- A-snail-enters-a-bar.js
+++ A-snail-enters-a-bar.js
@@ -34,3 +34,8 @@ Examples:
 
 
 // Solution
+
+function canSnailReachEnd(length, speed, lengthIncreases) {
+  return lengthIncreases < speed
+    && (length / (speed - lengthIncreases) < 365.25 * 24 * 60);
+}
